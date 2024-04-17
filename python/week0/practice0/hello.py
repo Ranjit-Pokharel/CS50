863182f884b49a920e8b3d374d4860e0451c7d27
Ranjit-Pokharel <rp.ranjitpokharel.pr@gmail.com>
2024-04-17 18:17:14 +0545

    programmed a user greet

---
+++ python/week0/practice0/hello.py
@@ -0,0 +1,54 @@
+"""
+Greet user as they pass their name.
+Author: Ranjit-Pokharel
+E-Mail: rp.ranjitpokharel.pr@gmail.com
+"""
+
+class Greet:
+    """
+    A class used for greeting users
+    ...
+    
+    Attributes
+    ----------
+    name : str
+        name of the user
+    
+    Methods
+    -------
+    hello()
+        Prints Hello with the user name
+    """
+
+    def __init__(self, name: str="world") -> None:
+        """
+        Parameters
+        ----------
+        name : str, optional
+            the name of the user (default is "world")
+        """
+
+        self.name: str = name.strip().title()
+
+    def hello(self) -> None:
+        """
+        Prints user name with hello
+        ...
+
+        Returns
+        -------
+        None
+        """
+
+        print(f"Hello, {self.name}")
+
+
+def main():
+    # get user name and clean the name
+    name: str = input("What's your name? ")
+    user: Greet = Greet(name)
+    user.hello()
+
+
+if __name__ == "__main__":
+    main()
